abdf4df4c8502727bfda5b15e37c8ff8dccc73d5
Nick <nick@arcanum.ai>
2024-10-15 14:45:56 +1300

    Removed npm lockfile and made Password Confirm

--- numa-frontend/src/Routes.jsx
+++ numa-frontend/src/Routes.jsx
@@ -1,18 +1,18 @@
 import { NumaLogin } from './pages/login';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 const NumaRoutes = () => {
-
-
   return (
     <Router>
-
-        <Routes>
-
-          <Route exact path="/login" element={<NumaLogin  />} />
-
-        </Routes>
-
+      <Routes>
+        <Route path="/login" element={<NumaLogin />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
     </Router>
   );
 };
