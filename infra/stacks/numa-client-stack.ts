0bdacc2abda579e5720cf33e1667470930adcc30
Dave Ball <dave@arcanum.ai>
2024-10-31 19:25:28 +0000

    refactor: Remove redundant client name.

--- infra/stacks/numa-client-stack.ts
+++ infra/stacks/numa-client-stack.ts
@@ -31,13 +31,11 @@ interface ClientConfig extends Omit<CoreNumaInfraProps, 'environmentName'> {
   apps?: Record<string, BaseNumaAppProps>;
 }
 // TODO: Replace this with some external store.
-const clientsProd: Record<string, ClientConfig> = {
+const clientsProd: Record<string, Omit<ClientConfig, 'client'>> = {
   accumen: {
-    client: 'accumen',
     clientAccountId: '677276116117',
   },
   'arcanum-demo': {
-    client: 'arcanum-demo',
     clientAccountId: '905418183804',
     createServiceLinkedRole: false,
     webCrawlerConfigs: [
@@ -47,31 +45,24 @@ const clientsProd: Record<string, ClientConfig> = {
     ],
   },
   'av-media': {
-    client: 'av-media',
     clientAccountId: '961341552812',
   },
   'beyond-expectations': {
-    client: 'beyond-expectations',
     clientAccountId: '474668383176',
   },
   'broken-hill-city-council': {
-    client: 'broken-hill-city-council',
     clientAccountId: '863518418134',
   },
   'design-builders': {
-    client: 'design-builders',
     clientAccountId: '061039773876',
   },
   'lg-pro': {
-    client: 'lg-pro',
     clientAccountId: '539247480075',
   },
   'modern-sales': {
-    client: 'modern-sales',
     clientAccountId: '228743762183',
   },
   nzsba: {
-    client: 'nzsba',
     clientAccountId: '307946678276',
     webCrawlerConfigs: [
       {
@@ -87,20 +78,16 @@ const clientsProd: Record<string, ClientConfig> = {
     loadSampleFile: false,
   },
   rooflogic: {
-    client: 'rooflogic',
     clientAccountId: '640168445517',
   },
   'story-box': {
-    client: 'story-box',
     clientAccountId: '851725285135',
     loadSampleFile: false,
   },
   tda: {
-    client: 'tda',
     clientAccountId: '575108948290',
   },
   'w-advisory': {
-    client: 'w-advisory',
     clientAccountId: '746669235417',
     webCrawlerConfigs: [
       {
@@ -119,12 +106,11 @@ const clientsProd: Record<string, ClientConfig> = {
     loadSampleFile: false,
   },
   'warrnambool-city-council': {
-    client: 'warrnambool-city-council',
     clientAccountId: '440744229592',
   },
 };
 
-const clientsDev: Record<string, ClientConfig> = {};
+const clientsDev: Record<string, Omit<ClientConfig, 'client'>> = {};
 
 export function listNumaClients(environmentName?: EnvironmentName): string[] {
   return Object.keys(environmentName == EnvironmentName.prod ? clientsProd : clientsDev);
@@ -132,7 +118,7 @@ export function listNumaClients(environmentName?: EnvironmentName): string[] {
 
 function lookupConfigForClient(client: string, environmentName?: EnvironmentName): ClientConfig {
   if (!listNumaClients(environmentName).includes(client)) throw new Error('Invalid client.');
-  return (environmentName == EnvironmentName.prod ? clientsProd : clientsDev)[client];
+  return { client, ...(environmentName == EnvironmentName.prod ? clientsProd : clientsDev)[client] };
 }
 
 export interface NumaClientStackProps extends ArcanumStackProps {
