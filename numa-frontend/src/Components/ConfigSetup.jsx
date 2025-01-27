51c9f0ded1b21a57835b2a27440e8f9b2df61ad5
Dave Ball <dave@arcanum.ai>
2025-01-27 16:19:19 +1300

    fix: Various improvements to fetching the config.

--- numa-frontend/src/Components/ConfigSetup.jsx
+++ numa-frontend/src/Components/ConfigSetup.jsx
@@ -1,44 +1,38 @@
 export const fetchConfigAddtoSession = async () => {
-  try {
-    await new Promise((resolve) => setTimeout(resolve, 100));
+  let needsRefresh = false;
 
-    const is_config = sessionStorage.getItem('Q_APPLICATION_ID', null);
+  const response = await fetch('/config.json', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    if (is_config === null) {
-      // TODO: change to load from s3 location
-
-      const response = await fetch('../config.json', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch manifest');
-      }
-
-      const configData = await response.json();
-
-      // Loop through the desired properties and add them to sessionStorage
-      const propertiesToAdd = [
-        'Q_APPLICATION_ID',
-        'Q_INDEX_ID',
-        'Q_DATASOURCE_ID',
-        'IDENTITY_POOL_ID',
-        'ROLE_ARN',
-        'REGION',
-        'API_ENDPOINT',
-        'USER_POOL_ID',
-        'CLIENT_ID',
-      ];
-      propertiesToAdd.forEach((property) => {
-        sessionStorage.setItem(property, configData[property]);
-      });
-
-      // reload so the items are available at login.
-      window.location.reload();
-    }
-  } catch (error) {
-    console.error('Error loading apps:', error);
+  if (!response.ok) {
+    throw new Error('Failed to fetch config');
   }
+
+  const configData = await response.json();
+
+  // Loop through the desired properties and add them to sessionStorage
+  const propertiesToAdd = [
+    'Q_APPLICATION_ID',
+    'Q_INDEX_ID',
+    'Q_DATASOURCE_ID',
+    'IDENTITY_POOL_ID',
+    'ROLE_ARN',
+    'REGION',
+    'API_ENDPOINT',
+    'USER_POOL_ID',
+    'CLIENT_ID',
+  ];
+  propertiesToAdd.forEach((property) => {
+    const existing = sessionStorage.getItem(property);
+    if ((configData[property] ?? 'undefined') != existing) {
+      sessionStorage.setItem(property, configData[property]);
+      needsRefresh = true;
+    }
+  });
+
+  // reload so the items are available at login.
+  if (needsRefresh) window.location.reload();
 };
