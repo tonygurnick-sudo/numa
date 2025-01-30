f372f8bc148d1328b48828e9f2618c9f6c0e49ec
hamshams <hamish@wairarapa.tech>
2025-01-30 02:20:39 +0000

    fix: the browser normalises the path, so altered the proxy so we can see how example apps

--- numa-frontend/vite.config.js
+++ numa-frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '^/../manifest.json': {
+      '/manifest.json': {
         target: 'http://localhost:5173',
         rewrite: () => '/src/Data/example-manifest.json'
       }
