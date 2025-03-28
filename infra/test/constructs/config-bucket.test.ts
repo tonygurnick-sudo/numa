31387531db2265f60f9d784aa0a2e7bba1fc925a
Dave Ball <dave@arcanum.ai>
2025-03-28 00:10:31 +0000

    test: ConfigBucket construct no longer includes creating file.

--- infra/test/constructs/config-bucket.test.ts
+++ infra/test/constructs/config-bucket.test.ts
@@ -23,25 +23,4 @@ describe('Config Bucket construct', () => {
       }),
     );
   });
-
-  it('Adds bucket object', (): void => {
-    const app = Testing.app();
-    const stack = new TestStack(app, 'test');
-    const synthesized = Testing.synth(stack);
-
-    assert(Testing.toHaveResource(synthesized, 'aws_s3_object'));
-
-    const s3Objects = JSON.parse(synthesized)['resource']['aws_s3_object'];
-    const s3ObjectList = Object.entries(s3Objects);
-    type Expected = {
-      key: string;
-      source: string;
-      bucket: string;
-    };
-    assert.equal(s3ObjectList.length, 1);
-    const [key, value] = s3ObjectList[0] as [string, Expected];
-    assert.match(key, /test_honeycomb-config-file_[A-F0-9]+/);
-    assert.equal(value['key'], 'config.yaml');
-    assert.match(value['source'], /.*\/infra\/assets\/otel-config\.yaml$/);
-  });
 });
