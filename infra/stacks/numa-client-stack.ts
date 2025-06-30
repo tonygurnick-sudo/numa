add315a522d2e80d697ac9122ccd6047696518b5
Dave Ball <dave@arcanum.ai>
2025-06-30 22:39:27 +0000

    fix: Properly check that we're preferring Q.

--- infra/stacks/numa-client-stack.ts
+++ infra/stacks/numa-client-stack.ts
@@ -64,7 +64,7 @@ export class NumaClientStack extends TerraformStack {
       ...props.clientConfig,
     };
 
-    if (!clientConfig.provisionQResources && clientConfig.preferredKnowledgeBase) {
+    if (!clientConfig.provisionQResources && clientConfig.preferredKnowledgeBase === 'q') {
       throw new Error('Nonsense detected: Q cannot be preferred knowledgebase when resources are not provisioned.');
     }
 
